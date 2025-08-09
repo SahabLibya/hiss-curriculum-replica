c3c0a8916705cd2c380cbf3e2f1823dac6f840c0
torgodly <torgodly@gmail.com>
2025-08-09 17:56:24 +0200

    Set basename for BrowserRouter to improve routing consistency

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/hiss-curriculum-replica">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
